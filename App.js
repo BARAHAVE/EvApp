8bf3503e2f274ec5fbd197499ce9b6085de6b1f7
barahave <richbabyface@hotmail.com>
2023-11-29 17:05:41 +0700

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { firebase } from '@react-native-firebase/auth';
 
-export default function App() {
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        navigation.navigate('Home');
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={setPassword}
+      />
+      <Button
+        title="Login"
+        onPress={handleLogin}
+      />
+      <Text style={styles.link}>
+        Don't have an account?
+        <Text onPress={() => navigation.navigate('Register')}>
+          Register
+        </Text>
+      </Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +52,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  input: {
+    width: 200,
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  link: {
+    fontSize: 16,
+    fontWeight: 'normal',
+    color: '#000',
+  },
 });
+
+export default LoginScreen;
